217612482cd07a3b313be2eb153127d190660146
thaminiperera <thaminiperera@gmail.com>
2025-02-27 14:46:44 +0530

    feat : home sidebar

--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavbar } from "../components/home-navbar";
+import { HomeSidebar } from "../components/home-sidebar";
 
 interface HomeLayoutProps {
   children: React.ReactNode;
@@ -10,7 +11,10 @@ export const HomeLayout = ({ children }: HomeLayoutProps) => {
     <SidebarProvider>
       <div className="w-full">
         <HomeNavbar />
-        <div className="">{children}</div>
+        <div className="flex min-h-screen pt-[4rem]">
+            <HomeSidebar/>
+          <main>{children}</main>
+        </div>
       </div>
     </SidebarProvider>
   );
