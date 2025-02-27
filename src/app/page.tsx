c9327eb1971fa1f1f2280f68dc3952b7d82cde19
thaminiperera <thaminiperera@gmail.com>
2025-02-27 12:41:33 +0530

    feat : logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-bold text-rose-500">
-      Hello World
+    <div className="">
+      <Image src="/logo.svg" height={50} width={50} alt="Logo" />{" "}
+      <p className="text-xl font-semibold tracking-tight">Cine</p>
     </div>
   );
 }
