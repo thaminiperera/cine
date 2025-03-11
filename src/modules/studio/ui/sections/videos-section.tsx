b56eec26b3c495fb84b6d22a366da8f8611330c9
thaminiperera <thaminiperera@gmail.com>
2025-03-11 12:21:56 +0530

    feat : studio video table

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,15 +1,71 @@
-"use client"
+"use client";
 
-import { DEFAULT_LIMIT } from "@/constants"
-import { trpc } from "@/trpc/client"
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+import Link from "next/link";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const VideosSection = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-        limit: DEFAULT_LIMIT
-    },{
-        getNextPageParam: (lastPage) => lastPage.nextCursor
-    })
-    return(
-        <div className="">{JSON.stringify(data)}</div>
-    )
-}
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return (
+    <div className="">
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead >Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages.flatMap((page)=> page.items).map((video) => (
+              <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
+                <TableRow>
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>visibility</TableCell>
+                  <TableCell>status</TableCell>
+                  <TableCell>date</TableCell>
+                  <TableCell>views</TableCell>
+                  <TableCell>comments</TableCell>
+                  <TableCell>likes</TableCell>
+                </TableRow>
+              </Link>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -1,7 +1,15 @@
-import { VideosSection } from "../sections/videos-section"
+import { VideosSection } from "../sections/videos-section";
 
 export const StudioView = () => {
-    return(
-        <div className=""><VideosSection/></div>
-    )
-}
+  return (
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Channel Content</h1>
+        <p className="text-sm text-muted-foreground">
+          Manage your channel content and videos
+        </p>
+      </div>
+      <VideosSection />
+    </div>
+  );
+};
