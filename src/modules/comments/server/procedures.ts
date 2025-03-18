a69698499eca6ce2d216f9afb96435e3d88a4d67
thaminiperera <thaminiperera@gmail.com>
2025-03-18 15:03:37 +0530

    feat : comment section

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,11 +1,11 @@
 import { db } from "@/db";
-import { comments } from "@/db/schema";
+import { comments, users } from "@/db/schema";
 import {
   baseProcedure,
   createTRPCRouter,
   protectedProcedure,
 } from "@/trpc/init";
-import { and, eq } from "drizzle-orm";
+import { and, eq, getTableColumns } from "drizzle-orm";
 import { z } from "zod";
 
 export const commentsRouter = createTRPCRouter({
@@ -32,9 +32,10 @@ export const commentsRouter = createTRPCRouter({
     .query(async ({ input }) => {
       const { videoId } = input;
       const data = await db
-        .select()
+        .select({ ...getTableColumns(comments), user: users })
         .from(comments)
-        .where(eq(comments.videoId, videoId));
+        .where(eq(comments.videoId, videoId))
+        .innerJoin(users, eq(comments.userId, users.id));
 
       return data;
     }),
--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CommentForm } from "@/modules/comments/ui/components/comment-form";
+import { CommentItem } from "@/modules/comments/ui/components/comment-item";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -22,12 +23,16 @@ export const CommentsSection = ({ videoId }: CommentSectionProps) => {
 const CommentsSectionSuspense = ({ videoId }: CommentSectionProps) => {
   const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
   return (
-    <div className="mt-6">
+    <div className="mt-6 mb-6">
       <div className="flex flex-col gap-6">
         <h1>0 Comments</h1>
         <CommentForm videoId={videoId} />
+        <div className="flex flex-col gap-4 mt-2">
+          {comments.map((comment) => (
+            <CommentItem key={comment.id} comment={comment} />
+          ))}
+        </div>
       </div>
-      {JSON.stringify(comments)}
     </div>
   );
 };
