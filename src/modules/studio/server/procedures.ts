18c4c6526f425f5ac78c55ae7c82a78a9a90b66c
thaminiperera <thaminiperera@gmail.com>
2025-04-02 18:20:27 +0530

    update : studio video section

--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 import { db } from "@/db";
-import { videos } from "@/db/schema";
+import { comments, users, videoReactions, videos, videoViews } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { eq, and, or, lt, desc } from "drizzle-orm";
+import { eq, and, or, lt, desc, getTableColumns } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 export const studioRouter = createTRPCRouter({
@@ -36,8 +36,17 @@ export const studioRouter = createTRPCRouter({
       const { cursor, limit } = input;
       const { id: userId } = ctx.user;
       const data = await db
-        .select()
+        .select({
+          ...getTableColumns(videos),
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          commentCount: db.$count(comments, eq(comments.videoId, videos.id)),
+          likeCount : db.$count(
+            videoReactions, and(eq(videoReactions.type, "like"), eq(videoReactions.videoId, videos.id))
+          ),
+          user: users
+        })
         .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
         .where(
           and(
             eq(videos.userId, userId),
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -144,9 +144,9 @@ export const VideosSectionSuspense = () => {
                     <TableCell className="text-sm truncate">
                       {format(new Date(video.createdAt), "d MMM yyyy")}
                     </TableCell>
-                    <TableCell className="text-right text-sm">views</TableCell>
-                    <TableCell className="text-right text-sm">comments</TableCell>
-                    <TableCell className="text-right text-sm pr-6">likes</TableCell>
+                    <TableCell className="text-right text-sm">{video.viewCount}</TableCell>
+                    <TableCell className="text-right text-sm">{video.commentCount}</TableCell>
+                    <TableCell className="text-right text-sm pr-6">{video.likeCount}</TableCell>
                   </TableRow>
                 </Link>
               ))}
